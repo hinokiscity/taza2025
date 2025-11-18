451ab0f5fa7d0941988fd41ae89c7976cc498f6b
Ajiniyaz Darmenbaev <142286187+hinokiscity@users.noreply.github.com>
2025-11-18 13:56:27 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,85 @@
+import os
+import asyncio
+from aiogram import Bot, Dispatcher, F, types
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.filters import Command
+
+
+TOKEN = os.getenv("BOT_TOKEN")
+if not TOKEN:
+    raise Exception("❌ BOT_TOKEN environment variable yo‘q!")
+
+
+bot = Bot(
+    token=TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
+
+dp = Dispatcher(storage=MemoryStorage())
+
+
+class Form(StatesGroup):
+    fio = State()
+    phone = State()
+    video = State()
+
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message, state: FSMContext):
+    await message.answer("Salom! FIO kiriting:")
+    await state.set_state(Form.fio)
+
+
+@dp.message(Form.fio)
+async def get_fio(message: types.Message, state: FSMContext):
+    await state.update_data(fio=message.text)
+    await message.answer("Telefon raqamingizni kiriting:")
+    await state.set_state(Form.phone)
+
+
+@dp.message(Form.phone)
+async def get_phone(message: types.Message, state: FSMContext):
+    await state.update_data(phone=message.text)
+    await message.answer("Endi videoni yuboring:")
+    await state.set_state(Form.video)
+
+
+@dp.message(Form.video, F.video.as_())
+async def get_video(message: types.Message, state: FSMContext):
+
+    data = await state.get_data()
+    fio = data["fio"]
+    phone = data["phone"]
+
+    caption = (
+        f"📌 <b>Yangi ishtirokchi!</b>\n"
+        f"👤 F.I.O: {fio}\n"
+        f"📞 Telefon: {phone}"
+    )
+
+    GROUP_ID = os.getenv("GROUP_ID")
+    if not GROUP_ID:
+        await message.answer("⚠️ GROUP_ID env yo‘q!")
+        return
+
+    await bot.send_video(
+        chat_id=int(GROUP_ID),
+        video=message.video.file_id,
+        caption=caption
+    )
+
+    await message.answer("Video yuborildi! Rahmat!")
+    await state.clear()
+
+
+async def main():
+    print("Bot ishga tushdi...")
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
